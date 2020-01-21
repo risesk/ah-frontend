a38461a3895afe3001341efb2776a17d50eb066b
risesk <antonkuvichka@gmail.com>
2020-01-22 01:23:11 +0300

    added async data load

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import React, {Component} from 'react';
 import './App.css';
 import { Table } from './Components/Table/Table'
 
-const profiles = require('./profiles.json');
-
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: profiles,
+      data: [],
       sort: '',  // 'asc' | 'desc'
       sortColumnName: '',
+      error: null,
+      isLoaded: false,
     };
     this.onSort = this.onSort.bind(this);
   }
@@ -18,12 +18,29 @@ class App extends Component {
   componentDidMount() {
     const appData = JSON.parse(localStorage.getItem('appData'))
     if (appData) {
-      this.setState({
+      return this.setState({
         data: appData.data,
         sort: appData.sort,
         sortColumnName: appData.sortColumnName,
+        isLoaded: true
       });
-    }
+    } else {
+        fetch('https://raw.githubusercontent.com/blmzv/ah-frontend-intern/master/profiles.json')
+          .then( res => res.json())
+          .then(
+            (res) => {
+              this.setState({
+                data: res,
+                isLoaded: true
+              });
+            },
+            (error) => {
+              this.setState({
+                isLoaded: true,
+                error
+              });
+            });
+      }
   }
 
   onSort(sortField) {
@@ -40,7 +57,7 @@ class App extends Component {
 
   sortProfiles(sortField, sort) {
     const cloneData = this.state.data.slice();
-    // Удаляем пробелы и скобки.
+    // Удаляем пробелы и скобки из сртируемого поля.
     const regExp = /[ \)\(]/g;
     const getSortParam = (profile) => {
       return profile[sortField].toLowerCase().replace(regExp, '');
@@ -56,18 +73,25 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div className='app'>
-        <h1 className='app__title'>Профили пользователей</h1>
-        <p className='app__text'>Кликни на заголовок колонки, чтобы отсортировать таблицу</p>
-        <Table
-          data={this.state.data}
-          sort={this.state.sort}
-          sortColumnName={this.state.sortColumnName}
-          onSort={this.onSort}
-        />
-      </div>
-    );
+    const { error, isLoaded } = this.state;
+    if (error) {
+      return <div>Ошибка: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Загрузка...</div>;
+    } else {
+      return (
+        <div className='app'>
+          <h1 className='app__title'>Профили пользователей</h1>
+          <p className='app__text'>Кликни на заголовок колонки, чтобы отсортировать таблицу</p>
+          <Table
+            data={this.state.data}
+            sort={this.state.sort}
+            sortColumnName={this.state.sortColumnName}
+            onSort={this.onSort}
+          />
+        </div>
+      );
+    }
   }
 }
 
